8aa0c86f5d2298d256d6e24de8970eaedb794069
janzibve-dot <janzibve@gmail.com>
2026-01-17 18:08:35 +0500

    Update physics.js

--- engine/physics.js
+++ engine/physics.js
@@ -4,17 +4,17 @@ export class Physics {
     }
 
     canMove(x, z) {
-        // Переводим мировые координаты в индексы массива (сетку)
         const gridX = Math.round(x);
         const gridZ = Math.round(z);
 
-        // Проверяем, не вышли ли за пределы массива
+        // Проверка границ массива
         if (this.map[gridZ] === undefined || this.map[gridZ][gridX] === undefined) {
             return false;
         }
 
-        // Если в этой клетке 1 — это стена
-        if (this.map[gridZ][gridX] === 1) {
+        // Нельзя ходить сквозь 1 (стены) и 2 (колонны)
+        const cell = this.map[gridZ][gridX];
+        if (cell === 1 || cell === 2) {
             return false;
         }
 
